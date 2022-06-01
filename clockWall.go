b20cdda938e1a04df39ecdd8b09a95b44855b7b0
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-05-31 19:22:02 -0700

    Update clockWall.go

--- clockWall.go
+++ clockWall.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+type clock struct {
+	place      string
+	connection net.Conn
+}
+
+func (c clock) handleConn() {
+	for {
+		data := make([]byte, 11)
+		_, err := c.connection.Read(data)
+		if err != nil {
+			c.connection.Close()
+			fmt.Printf("%s", err)
+			return
+
+		} else {
+			fmt.Printf("%s: %s", c.place, data)
+		}
+	}
+}
+
 func main() {
 	// Let's start the fun
+  if len(os.Args) == 1 {
+  fmt.Printf("Error")
+  return
+}
+for _, input := range os.Args[1:] {
+  data := strings.Split(input, "=")
+  conn, err := net.Dial("tcp", data[1])
+  if len(data) != 2 || err != nil {
+    fmt.Printf("Error\n")
+    return
+  }
+
+  go clock{
+    place:      data[0],
+    connection: conn,
+  }.handleConn()
+}
+for {}
 }
